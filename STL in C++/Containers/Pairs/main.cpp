c0243b9567683a078861c05e7aaf2b33aa62178b
Aditya Sharma <adisharma4857@gmail.com>
2025-07-24 17:16:01 +0530

    refactor: organise the folders

---
+++ STL in C++/Containers/Pairs/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    cout << "This a CPP test snippet" << endl;
+
+    pair <int, int> p = {1, 2};
+    cout << "The first element present in index 1 is: " << p.first;
+
+    return 0;
+}
