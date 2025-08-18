237e9bddcaa0f76d5a08ce3a0f681e3cf85579ea
Aditya Sharma <adisharma4857@gmail.com>
2025-08-18 08:39:18 +0530

    add: palindrome and printDivisorsOfANumber

---
+++ Basic Maths Prob/palindrome_no.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(int x) {
+        
+    while(x<0)
+    return 0;
+
+    int rem, ans=0, num=x;
+
+    while(num){
+        rem = num % 10;
+        num = num / 10;
+        if(ans > INT_MAX/10)
+        return 0;
+
+        ans = ans * 10 + rem;
+    }
+
+    if(ans==x)
+    return 1;
+    else
+    return 0;
+
+}
+
+int main()
+{
+    
+    return 0;
+}
---
+++ Basic Maths Prob/printAllFactors.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printAllFactors(int n) {
+    vector<int> ls;
+
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            ls.push_back(i);
+            
+            if ((n/i) != i) {
+                ls.push_back(n/i);
+            }
+        }
+    }
+
+    sort(ls.begin(), ls.end());
+
+    for (auto it : ls) cout << it << " ";
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "The factors of " << n << " are: ";
+    printAllFactors(n);
+    return 0;
+}
