cc8f9cede8ce5390704410cac46db9e0c8d0924a
Aditya Sharma <adisharma4857@gmail.com>
2025-09-20 21:51:42 +0530

    solved problems on arrays

---
+++ Arrays/Easy/moveAllZerosToTheEnd.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+    int n = nums.size();
+    int j = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+
+    if (j == -1) return;
+
+    for (int i = j + 1; i < n; i++) {
+        if (nums[i] != 0) {
+            swap(nums[i], nums[j]);
+            j++;
+        }
+    }
+
+}
+
+int main()
+{
+    vector<int> nums = {5,0,3,0,0,1,5};
+    int n = nums.size();
+
+    moveZeroes(nums);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
