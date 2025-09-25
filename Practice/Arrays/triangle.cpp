ad55a5a09b07ae8a9a5bb831f01662ba074b75dc
Aditya Sharma <adisharma4857@gmail.com>
2025-09-25 08:12:53 +0530

    practice potd array prob

---
+++ Practice/Arrays/triangle.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minimumTotal(vector<vector<int>>& triangle) {
+    int n = triangle.size();
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = 0; j < triangle[i].size(); j++) {
+            triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+        }
+    }
+
+    return triangle[0][0];
+}
+
+int main()
+{
+    vector<vector<int>> triangle = {
+        {2}, 
+       {3,4},
+      {6,5,7}, 
+     {4,1,8,3}
+    };
+
+    cout << "Minimum Total: " << minimumTotal(triangle);
+    return 0;
+}
