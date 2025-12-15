a8e6ac6c20f8ffb5c9b0ac8ba1b6c236ab50fb1b
Aditya Sharma <adisharma4857@gmail.com>
2025-12-15 07:54:29 +0530

    practiced upper bound in binary search

---
+++ Binary Search/upperBound.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int>& arr, int target) {
+    // code here
+    int low = 0;
+    int high = arr.size() - 1;
+    int ans = arr.size();
+    
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] > target) {
+            ans = mid;
+            high = mid - 1;
+        } 
+        else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    
+    return 0;
+}
